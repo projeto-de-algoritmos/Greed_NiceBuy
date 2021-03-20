2b064a56c220b8ea3a243e1d9dd0bea2d93139cb
Wagner Martins <wagnermc506@gmail.com>
2021-03-19 23:50:11 -0300

    Criando layout do site Co-Authored-By: Antonio Ruan <@RuanMoura>

--- nice_buy/src/App.js
+++ nice_buy/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className="App-header"></header>
+      <div className="App-body">
+        <div className="divider">
+          <div className="input-area">
+            <label for="nome" className="input-nome">Nome:</label>
+            <input type="text" id="nome" className="input-nome"></input>
+            <label for="preco" className="input-preco">Preço:</label>
+            <input type="number" id="preco" className="input-preco"></input>
+            <button>Adicionar</button>
+          </div>
+          <div className="show-bar">
+          </div>
+        </div>
+        <div className="divider">
+          <div className="show-items">
+            <div>Itens</div>
+            <table>
+              <tr>
+                <th>Nome</th>
+                <th>Preço</th>
+              </tr>
+              <tr>
+                <td>Kit Arduino</td>
+                <td>120.00</td>
+              </tr>
+              <tr>
+                <td>Playstation 5</td>
+                <td>5000.00</td>
+              </tr>
+              <tr>
+                <td>Playstation 5</td>
+                <td>5000.00</td>
+              </tr>
+              <tr>
+                <td>Playstation 5</td>
+                <td>5000.00</td>
+              </tr>
+              <tr>
+                <td>Playstation 5</td>
+                <td>5000.00</td>
+              </tr>
+              <tr>
+                <td>Playstation 5</td>
+                <td>5000.00</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

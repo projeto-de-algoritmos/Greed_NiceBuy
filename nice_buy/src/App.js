9d26f4843f28054da6c1c76a2ee0c651de95d936
Antonio Ruan Moura Barreto <ruanmoura13@outlook.com>
2021-03-21 13:22:46 -0300

    Adicionado orcamento e atributo preferencia para produto

--- nice_buy/src/App.js
+++ nice_buy/src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 function App() {
   const [name, setName] = useState('Default')
   const [preco, setPreco] = useState(999)
+  const [preferencia, setPreferencia] = useState(1)
   const [product, setProduct] = useState([])
+  const [orcamento, setOrcamento] = useState(0)
   
   const handleAdd = async() => {
-    await setProduct( previus => [...previus, [name, preco]] )
+    await setProduct( previus => [...previus, [name, preco, preferencia]] )
+    setPreferencia(1)
   }
 
   function ProductList(props){
@@ -16,6 +19,7 @@ function App() {
       <tr>
         <td>{product[0]}</td>
         <td>{product[1]}</td>
+        <td>{product[2]}</td>
       </tr>
     );
     return(
@@ -23,10 +27,21 @@ function App() {
         <tr>
           <th>Nome</th>
           <th>Preço (R$)</th>
+          <th>Preferência</th>
         </tr>
         {listItems}
       </table>
     );
+  }
+
+  function ListOptions(){
+    const list = [...Array(10).keys()].map(i => i+1)
+    const listOptions = list.map((option) =>
+      <option value={option}>
+        {option}
+      </option>
+    )
+    return listOptions
   }
 
   return (
@@ -35,10 +50,16 @@ function App() {
       <div className="App-body">
         <div className="divider">
           <div className="input-area">
+            <label for="orcamento" className="input-orcamento">Orçamento:</label>
+            <input type="number" id="orcamento" className="input-orcamento" onChange={(event) => {setOrcamento(Number(event.target.value))}}></input>
             <label for="nome" className="input-nome">Nome:</label>
             <input type="text" id="nome" className="input-nome" onChange={(event) => {setName(event.target.value)}}></input>
             <label for="preco" className="input-preco">Preço:</label>
             <input type="number" id="preco" className="input-preco" onChange={(event) => {setPreco(Number(event.target.value))}}></input>
+            <label for="preferencia" className="input-preferencia" id="preferencia-label">Preferência:</label>
+            <select id="preferencia" className="input-preferencia" value={preferencia} onChange={(event)=>{setPreferencia(event.target.value)}}>
+              <ListOptions></ListOptions>
+            </select>
             <button onClickCapture={ handleAdd } >Adicionar</button>
           </div>
           <div className="show-bar">

471efde4f99d0f732d4d9ec2b7567310e1c25cc1
Antonio Ruan Moura Barreto <ruanmoura13@outlook.com>
2021-03-21 03:26:11 -0300

    show-items modificado para ficar dinamico

--- nice_buy/src/App.js
+++ nice_buy/src/App.js
@@ -1,6 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('Default')
+  const [preco, setPreco] = useState(999)
+  const [product, setProduct] = useState([])
+  
+  const handleAdd = async() => {
+    await setProduct( previus => [...previus, [name, preco]] )
+  }
+
+  function ProductList(props){
+    const list = props.list;
+    const listItems = list.map((product) => 
+      <tr>
+        <td>{product[0]}</td>
+        <td>{product[1]}</td>
+      </tr>
+    );
+    return(
+      <table>
+        <tr>
+          <th>Nome</th>
+          <th>Preço (R$)</th>
+        </tr>
+        {listItems}
+      </table>
+    );
+  }
+
   return (
     <div>
       <header className="App-header"></header>
@@ -8,47 +36,17 @@ function App() {
         <div className="divider">
           <div className="input-area">
             <label for="nome" className="input-nome">Nome:</label>
-            <input type="text" id="nome" className="input-nome"></input>
+            <input type="text" id="nome" className="input-nome" onChange={(event) => {setName(event.target.value)}}></input>
             <label for="preco" className="input-preco">Preço:</label>
-            <input type="number" id="preco" className="input-preco"></input>
-            <button>Adicionar</button>
+            <input type="number" id="preco" className="input-preco" onChange={(event) => {setPreco(Number(event.target.value))}}></input>
+            <button onClickCapture={ handleAdd } >Adicionar</button>
           </div>
           <div className="show-bar">
           </div>
         </div>
         <div className="divider">
           <div className="show-items">
-            <div>Itens</div>
-            <table>
-              <tr>
-                <th>Nome</th>
-                <th>Preço</th>
-              </tr>
-              <tr>
-                <td>Kit Arduino</td>
-                <td>120.00</td>
-              </tr>
-              <tr>
-                <td>Playstation 5</td>
-                <td>5000.00</td>
-              </tr>
-              <tr>
-                <td>Playstation 5</td>
-                <td>5000.00</td>
-              </tr>
-              <tr>
-                <td>Playstation 5</td>
-                <td>5000.00</td>
-              </tr>
-              <tr>
-                <td>Playstation 5</td>
-                <td>5000.00</td>
-              </tr>
-              <tr>
-                <td>Playstation 5</td>
-                <td>5000.00</td>
-              </tr>
-            </table>
+            <ProductList list={product}/>
           </div>
         </div>
       </div>
